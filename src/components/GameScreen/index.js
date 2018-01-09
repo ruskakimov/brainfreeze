e4b80f6ecea63b8f802541701c2dcfe6533b3bc6
Rustem Kakimov <ruskakimov@gmail.com>
2018-01-09 12:37:34 +0600

    add game timer

--- src/components/GameScreen/index.js
+++ src/components/GameScreen/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const GameScreen = ({ goToMenuScreen }) => {
-  return (
-    <div>
-      game screen <button onClick={goToMenuScreen}>menu</button>
-    </div>
-  );
-};
+class GameScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: 60
+    };
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer = window.setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState(prevState => {
+      return { secondsLeft: prevState.secondsLeft - 1 };
+    });
+  }
+
+  render() {
+    const { goToMenuScreen } = this.props;
+    return (
+      <div>
+        {this.state.secondsLeft}
+        game screen <button onClick={goToMenuScreen}>menu</button>
+      </div>
+    );
+  }
+}
 
 export default GameScreen;
